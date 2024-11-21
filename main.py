5d827ac2890f846129ac7dc336cfef3fba665dbd
0kogu <nascimentojosue2002@gmail.com>
2024-11-21 09:13:01 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+import string
+
+# Download NLTK resources
+nltk.download('punkt')
+nltk.download('stopwords')
